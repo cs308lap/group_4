a77ceb21bd58bfeb6e1750e7207679abc481da80
smile003 <shashwatsingh1892@gamil.com>
2021-08-23 15:23:48 +0530

    step 5 completed

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <stdio.h>
 #include "functions.h"
 using namespace std;
 int main(){
     print_hello();
     cout<< endl;
-    printf("The factorial of 5 is %d\n", factorial(5));
+    cout <<"The factorial of 5 is " <<factorial(5)<<endl;
     return 0;
 }
